d4999ec33f1ae1af9fe20a2e36d4bf343af77c33
Hualdo <hucastillo@gmail.com>
2022-11-05 10:20:20 -0500

    Final Version, enjoy it ...

---
+++ notification.js
@@ -0,0 +1,16 @@
+let mark = document.getElementById('mark')
+let comment = document.getElementsByClassName('comment')
+let circle = document.getElementsByClassName('circle')
+let number = document.getElementById('number')
+
+mark.addEventListener('click',()=>{
+   for(i=0;i<comment.length;i++){
+    comment[i].style.backgroundColor='white'    
+    }
+
+   for(i=0;i<circle.length;i++){
+    circle[i].style.display='none'
+   } 
+   console.log(number)
+   number.innerHTML='0'
+})
